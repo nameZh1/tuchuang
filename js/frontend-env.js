7aadf93d4f5d62585c69557cf2e57cb87b3a1b9a
zh1 <834197886@qq.com>
2023-10-25 05:23:04 +0800

    11

--- js/frontend-env.js
+++ js/frontend-env.js
@@ -1,6 +1,2 @@
 // window.GITHUB_TOKEN = '${{ secrets.TUCHUANGTOKEN }}';
-<<<<<<< HEAD
 console.log("🚀 ~ file: frontend-env.js:2 ~ window.GITHUB_TOKEN:", window.GITHUB_TOKEN)
-=======
-// console.log("🚀 ~ file: frontend-env.js:2 ~ window.GITHUB_TOKEN:", window.GITHUB_TOKEN)
->>>>>>> 8d68034bc792afe467ac8a07645fb272d81eeed4
